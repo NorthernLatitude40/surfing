de93862638c81dfe662283513e099a6ce3ee82fd
ww <wang_123333@sina.com>
2019-10-18 00:03:37 +0800

    add

---
+++ src/main/java/com/cc/haha/Apple.java
@@ -0,0 +1,10 @@
+package com.cc.haha;
+
+public class Apple {
+
+    private String apple = "GOOD APPLE";
+
+    public String getApple() {
+        return apple;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<%@ page import="com.cc.haha.Apple" %>
+<html>
+<body>
+<h2>Hello World!</h2>
+<%= new Apple().getApple()%>
+</body>
+</html>
